19db2e8799d1566830ec63420976c4c24b33071b
José Ramón <j.ramonroz@gmail.com>
2024-05-07 21:51:13 -0600

    feat: server is now open for connections!

--- src/app/server.py
+++ src/app/server.py
@@ -1,13 +1,42 @@
 from textual.app import App, ComposeResult
-from textual.containers import ScrollableContainer
-from textual.widgets import Header, Footer, LoadingIndicator, Button, Label,Static
+from textual.containers import ScrollableContainer, Container
+from textual.widgets import Header, Footer, LoadingIndicator, Label, Static
+import os
+import socket
+import threading as thrd
 
 
 class PantallaInicial(Static):
     """Widget de espera de Conexion"""
+
+    def getIpAddress(self) -> str:
+        #TODO: Implement this method but for Windows
+        """Gets the IP address of the host"""
+        ipv4 = os.popen('ip addr').read().split("inet ")[2].split("/")[0]
+        return ipv4
+
+    def awaitingConnection(self):
+        s = socket.socket()
+        port = 1337
+        s.bind((self.getIpAddress(), port))
+        s.listen(5)
+        c, addr = s.accept()
+        if addr:
+            print("Conexión establecida con: ", addr)
+            c.close()
+            #TODO: Implement this method
+
     def compose(self) -> ComposeResult:
-        yield Label("Esperando una conexión", id="estadoConexion")
-        yield LoadingIndicator()
+        urName = self.getIpAddress()
+        yield Container(
+            Label("ESPERANDO UNA CONEXIÓN", classes="info", id="estadoConexion"),
+            Label("IP: " + urName, classes="info", id="hostIp"),
+            Label("PUERTO: 1337", classes="info", id="hostPort"),
+            LoadingIndicator(id="indicadorEspera"),
+            id="contenedorInicial",
+        )
+        thrd.Thread(target=self.awaitingConnection).start()
+
 
 class serverApp(App):
     """Manejo de la aplicacion"""
@@ -17,13 +46,14 @@ class serverApp(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield ScrollableContainer(PantallaInicial())
+        yield Container(PantallaInicial())
 
     def action_toggle_dark(self) -> None:
         self.dark = not self.dark
 
     def action_quit(self) -> None:
         quit()
+
 
 
 if __name__ == "__main__":
